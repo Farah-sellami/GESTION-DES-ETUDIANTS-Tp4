1ae01e35aed2edacc754c7a93e29feef09efb91e
farah <farah.sell451@gmail.com>
2022-05-20 05:41:55 +0100

    commit

---
+++ Models/Repositories/ISchoolRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationSchool.Models;
+
+namespace WebApplicationSchool.Repositories
+{
+    public interface ISchoolRepository
+    {
+        IList<School> GetAll();
+        School GetById(int id);
+        void Add(School s);
+        void Edit(School s);
+        void Delete(School s);
+        double StudentAgeAverage(int schoolId);
+        int StudentCount(int schoolId);
+    }
+}
